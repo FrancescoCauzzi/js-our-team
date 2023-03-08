e1339467dbbaeb94f4127a3a6a3623d6552fb395
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-03-08 16:35:47 +0100

    feat: bonus 1 e 2 completati

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let myOutputEl = document.querySelector(".__output");
+let myOutputUlEl = document.querySelector(".__output-ul");
+let myOutputDiv = document.querySelector(".__output-div");
 /*
 Wayne Barnett	  Founder & CEO	         wayne-barnett-founder-ceo.jpg
 Angela Caroll	  Chief Editor	         angela-caroll-chief-editor.jpg
@@ -85,4 +86,40 @@ function printTeamInfoInPage(team, containerEl) {
 }
 
 printTeamInfo(team);
-printTeamInfoInPage(team, myOutputEl);
+printTeamInfoInPage(team, myOutputUlEl);
+// Creo una funzione che mi genera le mie 6 cards
+{
+  /* <div class="card" >
+  <img src="" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
+  </div>
+</div> */
+}
+
+function generateCards(arr, containerEl) {
+  for (let i = 0; i < arr.length; i++) {
+    let myCardDiv = document.createElement("div");
+    myCardDiv.classList.add("card");
+    myCardDiv.style.width = `calc(100% / 3 * 1 - (20px / 3 *2));`;
+    let img = document.createElement("img");
+    img.setAttribute("src", `img/${arr[i].image}`);
+    myCardDiv.append(img);
+    containerEl.append(myCardDiv);
+    let cardBodyEl = document.createElement("div");
+    cardBodyEl.classList.add("card-body");
+    cardBodyEl.style.textAlign = "center";
+    myCardDiv.append(cardBodyEl);
+    let cardBodyTitleEl = document.createElement("h5");
+    cardBodyTitleEl.innerHTML = `${arr[i].name}`;
+    cardBodyEl.append(cardBodyTitleEl);
+    let cardBodySubTitle = document.createElement("h6");
+
+    cardBodySubTitle.innerHTML = `${arr[i].role}`;
+    cardBodyEl.append(cardBodySubTitle);
+  }
+}
+
+generateCards(team, myOutputDiv);
